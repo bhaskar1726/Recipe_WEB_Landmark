e776f38c6e2d03e472bbe8df22cd740d3a3aa1ca
Bhaskar <bhaskarkumar3111@gmail.com>
2022-06-27 20:43:09 +0530

    Initial commit

---
+++ recepi.js
@@ -0,0 +1,16 @@
+getRandomMeal();
+
+async function getRandomMeal(){
+    const resp =await  fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+    
+    const randomMeal = await resp.jason();
+    console.log(randomMeal);
+}
+
+async function getMealById(id){
+    const meal =await fetch("www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
+}
+
+async function getMealsBySearch(term){
+    const meals =await fetch("www.themealdb.com/api/json/v1/1/search.php?s=" + term);
+} 
